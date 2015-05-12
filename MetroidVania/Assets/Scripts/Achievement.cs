1237895436f121d5909bed93ee008fc2ae3ef535
jos_boon <josejboon@gmail.com>
2015-05-12 09:20:22 +0200

    added JoseTestScene

---
+++ MetroidVania/Assets/Scripts/Achievement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Achievement : MonoBehaviour, IObserver {
+	public bool unlocked 	= false;
+	public string name 		= "";
+	public float showTimer 	= 3;
+	public int speed 		= 100;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(unlocked)
+		{
+			//Show Achievement
+		}
+	}
+
+	public void notify()
+	{
+		unlock();
+	}
+
+	void unlock()
+	{
+		unlocked = true;
+	}
+
+}
