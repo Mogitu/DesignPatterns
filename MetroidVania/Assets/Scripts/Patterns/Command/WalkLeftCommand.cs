e65951384838c2333a850856d62b38fe09c5873c
Maikel <mogitu@msn.com>
2015-05-16 14:16:24 +0200

    *added folders to make it easier to distinguish scripts by patterns *added player animation(works with the mecanim STATE machine) *added gamescene as the scene to contain the "real" game *player class caches animation, rigidbody2d, etc to demonstrate the component model.

---
+++ MetroidVania/Assets/Scripts/Patterns/Command/WalkLeftCommand.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WalkLeftCommand : Command
+{
+	public override void Execute (GameObject obj)
+	{
+		Player script = obj.GetComponent<Player> ();
+		obj.transform.localScale = new Vector2 (-1, 1);
+		script.Animator.SetBool("walking",true);
+		script.ForceX = -script.walkSpeed;
+	}
+}
--- MetroidVania/Assets/Scripts/Player.cs
+++ MetroidVania/Assets/Scripts/Player.cs
@@ -2,24 +2,42 @@
 using System.Collections;
 
 public class Player : MonoBehaviour {
-	public InputHandler inputHandler;
+	public int health=100;
+	public float walkSpeed=2;
+	public float jumpPower=300;
 	private float forceX;
+
+	//These objects can be seen as a part of the component structure
+	private InputHandler inputHandler;
 	private Rigidbody2D body;
+	private Animator animator;
 
 	// Use this for initialization
 	void Start () {
+		//here we assign/cache the components.
 		inputHandler = new InputHandler();
 		body = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator>();
+	}
+
+	void FixedUpdate(){
+		/* at start of each physics frame we set several, physics related, values to 0 
+           for when there is no input etc.
+		 */		 
+		forceX=0;
+		animator.SetBool("walking",false);
+		Command command = inputHandler.HandleInput();
+		//check if there is ANY type of command being executed.
+		if(command!=null){
+			command.Execute(gameObject);
+		}
+		//we update the speed depending on values we get from the input
+		body.velocity = new Vector2(forceX, body.velocity.y);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		forceX=0;
-		Command command = inputHandler.HandleInput();
-		if(command!=null){
-			command.Execute(gameObject);
-		}
-		body.velocity = new Vector2(forceX, body.velocity.y);
+
 	}
 
 
@@ -27,5 +45,9 @@ public class Player : MonoBehaviour {
 	{
 		get {return forceX;}
 		set { forceX = value;}
+	}
+
+	public Animator Animator{
+		get {return animator;}
 	}
 }
