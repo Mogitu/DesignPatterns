71a4627158d7467839a27034d5f5b61cc375eb40
Maikel <mogitu@msn.com>
2015-05-18 12:23:00 +0200

    *flyweight test objects in maikel test scene

---
+++ MetroidVania/Assets/Scripts/Patterns/FlyWeight/FlyWeightObj.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyWeightObj : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject obj;
+	private SpriteRenderer rend;
+	private SpriteRenderer other;
+	void Start () {
+	    rend = GetComponent<SpriteRenderer>();
+		other = obj.GetComponent<SpriteRenderer>();
+		rend.sprite = other.sprite;		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
