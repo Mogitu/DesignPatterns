e63cb51ceb882a8a4c7970d2fd96aa86249598b5
basabas <basvanrijn901@hotmail.com>
2015-05-18 16:54:01 +0200

    Added Prototype pattern. You can now shoot an arrow. The player(prefabs->Player(Bas)) can shoot walk etc, and ofcourse it has checkpoints. Player also has a new appearence, it is an archer.

---
+++ MetroidVania/Assets/Scripts/Patterns/Prototype/ArrowBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowBehaviour : MonoBehaviour {
+
+	public float speed = 30;
+	private bool hasLanded = false;
+	Rigidbody2D body;
+	BoxCollider2D collider;
+	// Use this for initialization
+	void Start () {
+		body = GetComponent<Rigidbody2D>();
+		collider = GetComponent<BoxCollider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!hasLanded)
+		{
+			transform.position = transform.position + transform.right * speed* Time.deltaTime;
+			Vector3 rot = transform.eulerAngles;
+			if(rot.z<90)rot.z-= 30*Time.deltaTime;
+			else if(rot.z<180)rot.z+=30*Time.deltaTime;
+			else if(rot.z<270)rot.z+=60*Time.deltaTime;
+			else rot.z-=60*Time.deltaTime;
+			transform.eulerAngles = rot;
+		}
+
+	}
+
+	public void OnCollisionEnter2D(Collision2D collider)
+	{
+		if(hasLanded)return;
+		if(collider.gameObject.name=="Player")
+			GameManager.restoreCheckPoint();
+		else
+		{
+			hasLanded = true;
+			body.isKinematic = true;
+			this.collider.enabled = false;
+			transform.parent = collider.transform;
+		}
+	}
+}
