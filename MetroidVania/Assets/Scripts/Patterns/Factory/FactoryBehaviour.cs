bfbdcbc68fbf8d25663fac239e8002157c37f936
Dimitri Meister <dimitri.meister@hva.nl>
2015-05-22 15:47:04 +0200

    Changed how props are changed

--- MetroidVania/Assets/Scripts/Patterns/Factory/FactoryBehaviour.cs
+++ MetroidVania/Assets/Scripts/Patterns/Factory/FactoryBehaviour.cs
@@ -1,11 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TestScript : MonoBehaviour {
-	propsCreator creator;public float testFloat = 6;
-	[Range(0,30)]
-	public float testFloat2 = 6;
-	public string testString = "tesetets";
+public class FactoryBehaviour : MonoBehaviour {
+	propsCreator creator;
 	public Sprite[] propSprites;
 	// Use this for initialization
 	void Start () {
@@ -14,14 +11,8 @@ public class TestScript : MonoBehaviour {
 		creator.setPropSprites(propSprites);
 		for (int i = 0; i < tempProps.Length; i++)
 		{
-			
 			creator.FactoryMethod (tempProps[i].transform.position,tempProps[i].transform.localScale);
 			Destroy(tempProps[i]);
 		}
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
 	}
 }
